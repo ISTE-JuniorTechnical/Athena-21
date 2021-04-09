856eff29b6a73eacb789fd552aa3d4d9cdab065d
webdesign4alll <asbupsilonviroidno1@gmail.com>
2021-04-09 15:01:45 +0530

    swiper js added in events html events js

--- event.js
+++ event.js
@@ -1,10 +1,23 @@
-if(window.innerWidth <= 768 + 'px'){
+var innerwidth = window.innerWidth;
+var expectedwidth = 768;
+console.log(innerwidth);
+console.log(expectedwidth)
+
+
+
+if(innerwidth <= expectedwidth){
+
     for(let i = 0; i < 4; i++){
         let mainrow = document.getElementsByClassName('mainrow')[i];
         let subrow = document.getElementsByClassName('subrow1')[i];
-        let innerrow = document.getElementsByClassName('innerrow')[i];
         mainrow.classList.add('swiper-container');
         subrow.classList.add('swiper-wrapper');
+    }
+
+    for (let j = 0; j < 12; j++){
+
+        innerrow = document.getElementsByClassName('innerrow')[j];
         innerrow.classList.add('swiper-slide');
+        
     }
 }
